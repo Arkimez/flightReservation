623878448add1930859e951d42824a0c162d6970
Den Hakim <denn.hakimm@gmail.com>
2025-12-23 09:01:19 +0800

    bug

--- home.java
+++ home.java
@@ -13,15 +13,15 @@ public class home {
 		break;
       }
       case 2: {
-		System.out.println("1");
+		System.out.println("2");
 		break;
       }
      case 3: {
-		System.out.println("1");
+		System.out.println("3");
 		break;
       }
      case 4: {
-		System.out.println("1");
+		System.out.println("4");
 		break;
       }
     }
